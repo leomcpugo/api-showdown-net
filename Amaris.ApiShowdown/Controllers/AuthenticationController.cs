2e74891293599117a58ac15133496441420bf3e1
Leonardo Mata <leonardo.matarengifo@amaris.com>
2018-05-24 17:47:27 +0200

    Added authentication JWT

--- Amaris.ApiShowdown/Controllers/AuthenticationController.cs
+++ Amaris.ApiShowdown/Controllers/AuthenticationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Amaris.ApiShowdown.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,9 +13,23 @@ namespace Amaris.ApiShowdown.Controllers
     public class AuthenticationController : Controller
     {
         // POST api/values
-        [HttpPost("login")]
-        public void Post([FromBody]string id)
+        [HttpPost]
+        public async Task<ContentResult> Post([FromBody]AuthenticationForm form)
         {
+            var userList = await new UserService().GetUsers();
+
+            var response = new ContentResult
+            {
+                Content = new AuthenticationService().SignToken(userList.FirstOrDefault(x => x.id == form.id)),
+                StatusCode = 200
+            };
+
+            return await Task.FromResult(response);
+        }
+
+        public class AuthenticationForm
+        {
+            public string id { get; set; }
         }
     }
 }
--- Amaris.ApiShowdown/Services/AuthenticationService.cs
+++ Amaris.ApiShowdown/Services/AuthenticationService.cs
@@ -1,15 +1,41 @@
-﻿using System;
+﻿using Amaris.ApiShowdown.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Amaris.ApiShowdown.Services
 {
     public class AuthenticationService
     {
-        public void SignToken(string id, string role)
+        private const string Secret = "db3OIsj+BXE9NZDy0t8W3TcNekrF+2d/1sFnWG4HnV8TZY30iTOdtVWJG8abWvB1GlOgJuQZdcF2Luqm/hccMw==";
+        private const int expireMinutes = 60;
+
+        public string SignToken(User user)
         {
-            //System.IdentityModel.Tokens.Jwt.
+            var symmetricKey = Convert.FromBase64String(Secret);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var now = DateTime.UtcNow;
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                        {
+                        new Claim(ClaimTypes.Sid, user.id)
+                    }),
+
+                Expires = now.AddMinutes(Convert.ToInt32(expireMinutes)),
+
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(symmetricKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var stoken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(stoken);
+
+            return token;
         }
     }
 }
